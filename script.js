8425d5418967fcdeac89734889c4e6b201580bf5
denizdogruel00 <denizyazilm@gmail.com>
2026-02-04 18:57:36 +0300

    butona tıklama eklendi

---
+++ script.js
@@ -0,0 +1,9 @@
+let button=document.getElementById("btnkaydet");
+let ad=document.getElementById("adi").value.trim();
+let tarih=document.getElementById("Tarih").value.trim();
+let aciklama=document.getElementById("aciklama").value.trim();
+let table=document.querySelector("table");
+button.addEventListener("click",function(){
+    
+
+})
