bcb6dfa84b02e5a5b288f910ab53a994ecb23f8b
denizdogruel00 <denizyazilm@gmail.com>
2026-02-04 19:12:42 +0300

    proje final

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
-let button=document.getElementById("btnkaydet");
-let ad=document.getElementById("adi").value.trim();
-let tarih=document.getElementById("Tarih").value.trim();
-let aciklama=document.getElementById("aciklama").value.trim();
-let table=document.querySelector("table");
-button.addEventListener("click",function(){
+let button = document.getElementById("btnkaydet");
+
+
+button.addEventListener("click", function () {
+    let table = document.querySelector("table");
+    let ad = document.getElementById("adi").value.trim();
+    let tarih = document.getElementById("Tarih").value.trim();
+    let aciklama = document.getElementById("aciklama").value.trim();
+  
     
+    let tr = document.createElement("tr");
+   
+    let td1 = document.createElement("td");
+    let td2 = document.createElement("td");
+    let td3 = document.createElement("td");
+    td1.textContent=ad;
+    td2.textContent=tarih;
+    td3.textContent=aciklama;
+
+     tr.appendChild(td1)
+     tr.appendChild(td2)
+     tr.appendChild(td3)
+    table.appendChild(tr)
+   
+
+
+   
+   
+
+
 
 })
